2ead7c52d8018846b43a1aff647791b59bcadac2
Nikhil K Singh <37348724+Nikhil-K-Singh@users.noreply.github.com>
2025-08-11 00:29:03 +0530

    included yt-dlp

--- setup_macbook.sh
+++ setup_macbook.sh
@@ -2,6 +2,7 @@ brew install pipx rust eza tree bat thefuck entr tldr vlc motrix
 brew install --cask visual-studio-code
 
 pipx install quickdownload
+pipx install yt-dlp
 
 cargo install --locked zellij
 
