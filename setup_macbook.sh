b1538f531d0e37712d287725f08742cbb97d8f3b
Nikhil K Singh <37348724+Nikhil-K-Singh@users.noreply.github.com>
2025-08-17 15:00:09 +0530

    Update setup_macbook.sh
    path variables were missing

--- setup_macbook.sh
+++ setup_macbook.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/zsh
 
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH=/opt/homebrew/bin:$PATH 
+export PATH=/opt/homebrew/bin:$PATH
+
+# Install command-line tools
 brew install pipx rust eza tree bat thefuck entr tldr vlc ffmpeg motrix ripgrep zsh pure fzf
 brew install zsh-autosuggestions zsh-syntax-highlighting
-python3 -m pipx ensurepath
 
-brew install --cask visual-studio-code firefox
+export PATH="$HOME/.local/bin:$PATH"
 
+# Install downloading tools
 pipx install quickdownload
 pipx install yt-dlp
 
-cargo install --locked zellij
+# Install GUI applications
+brew install --cask visual-studio-code firefox
 
+# Install Zellij
+cargo install --locked zellij
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Build FilePilot
 git clone https://github.com/Nikhil-K-Singh/FilePilot.git
 cd FilePilot
 cargo build --release
 cd ..
 
-#DOCKER: 2 APPROACHES
+#DOCKER
 # if DOCKER installation fails to pull packages check the DNS on your wifi connection from your system settings,
 # set DNS to 8.8.8.8 and 1.1.1.1
 brew install --cask docker
 
 #GITHUB DESKTOP
-qd https://central.github.com/deployments/desktop/desktop/latest/darwin-arm64 -o "GithubDesktop.zip"
+quickdownload https://central.github.com/deployments/desktop/desktop/latest/darwin-arm64 -o "GithubDesktop.zip"
 open GithubDesktop.zip
