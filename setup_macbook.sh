c617d5f4d0066978b8fe158df8de4df424470442
Nikhil K Singh <37348724+Nikhil-K-Singh@users.noreply.github.com>
2025-09-05 00:05:11 +0530

    Enhance setup script with additional app installations
    Added installation commands for Raycast, Draw.io, GitHub Desktop, and Docker. Removed commented Docker installation instructions.

--- setup_macbook.sh
+++ setup_macbook.sh
@@ -15,7 +15,11 @@ pipx install quickdownload
 pipx install yt-dlp
 
 # Install GUI applications
-brew install --cask visual-studio-code firefox
+brew install --cask visual-studio-code firefox raycast drawio github docker 
+
+# if DOCKER installation fails to pull packages check the DNS on your wifi connection from your system settings,
+# set DNS to 8.8.8.8 and 1.1.1.1
+
 
 # Install Zellij
 cargo install --locked zellij
@@ -33,10 +37,16 @@ fi
 cargo build --release
 cd ..
 
-# DOCKER
-# if DOCKER installation fails to pull packages check the DNS on your wifi connection from your system settings,
-# set DNS to 8.8.8.8 and 1.1.1.1
-brew install --cask docker
+cd ~
+if [ -d "scribble" ]; then
+  cd scribble
+  git pull
+else
+  git clone https://github.com/Nikhil-K-Singh/scribble.git
+  cd scribble
+fi
+cargo build --release
+cd ..
 
 # brew install firefoxpwa
 # firefoxpwa runtime install
@@ -53,11 +63,4 @@ brew install --cask docker
 #   "https://web.whatsapp.com/data/manifest.json" 
 
 
-# Desktop Applications
-quickdownload "https://shottr.cc/dl/Shottr-1.8.1.dmg" -o "Shottr.dmg" -p 8
-quickdownload "https://github.com/jgraph/drawio-desktop/releases/download/v28.0.6/draw.io-universal-28.0.6.dmg" -o "drawio.dmg" -p 8
-quickdownload "https://central.github.com/deployments/desktop/desktop/latest/darwin-arm64" -o "GithubDesktop.zip" -p 8
-open "Shottr.dmg"
-open  "drawio.dmg"
-open "GithubDesktop.zip"
 exit
